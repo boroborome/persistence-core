0ed33d2bd184ecaf936bd6b9451b73d0174385d4
Yushan Gao <ysgao@thoughtworks.com>
2020-11-07 12:24:31 +0800

    refactor: clean code

--- src/main/java/com/happy3w/persistence/core/rowdata/page/IReadDataPage.java
+++ src/main/java/com/happy3w/persistence/core/rowdata/page/IReadDataPage.java
@@ -1,13 +1,8 @@
 package com.happy3w.persistence.core.rowdata.page;
 
 import com.happy3w.persistence.core.rowdata.ExtConfigs;
-import com.happy3w.persistence.core.rowdata.IRdTableDef;
-import com.happy3w.persistence.core.rowdata.RdRowWrapper;
-
-import java.util.stream.Stream;
 
 public interface IReadDataPage<T extends IReadDataPage<T>> extends IDataPage<T> {
 
     <D> D readValue(int rowIndex, int columnIndex, Class<D> dataType, ExtConfigs extConfigs);
-
 }
