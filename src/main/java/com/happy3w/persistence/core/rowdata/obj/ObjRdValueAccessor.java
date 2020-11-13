4b328fc4003218c368499f3032d9a7d127ef3718
Yushan Gao <ysgao@thoughtworks.com>
2020-11-13 20:09:36 +0800

    refactor: init ObjRdValueAccessor with FieldAccessor

--- src/main/java/com/happy3w/persistence/core/rowdata/obj/ObjRdValueAccessor.java
+++ src/main/java/com/happy3w/persistence/core/rowdata/obj/ObjRdValueAccessor.java
@@ -1,10 +1,9 @@
 package com.happy3w.persistence.core.rowdata.obj;
 
+import com.happy3w.toolkits.reflect.FieldAccessor;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.beans.IntrospectionException;
-import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -18,13 +17,11 @@ public class ObjRdValueAccessor {
     private Method setMethod;
 
     public static ObjRdValueAccessor from(Field field) {
-        try {
-            PropertyDescriptor desc = new PropertyDescriptor(field.getName(), field.getDeclaringClass());
-            return new ObjRdValueAccessor(field.getName(), field.getType(),
-                    desc.getReadMethod(), desc.getWriteMethod());
-        } catch (IntrospectionException e) {
-            throw new RuntimeException("No Property define for field:" + field.toString(), e);
-        }
+        FieldAccessor fieldAccessor = FieldAccessor.from(field);
+        return new ObjRdValueAccessor(fieldAccessor.getFieldName(),
+                fieldAccessor.getDataType(),
+                fieldAccessor.getGetMethod(),
+                fieldAccessor.getSetMethod());
     }
 
     public Object getValue(Object data) {
