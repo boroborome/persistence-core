81d77237dc8aed1a4d5092a4c7ba7a234cc5e8e2
Yushan Gao <ysgao@thoughtworks.com>
2020-11-22 12:34:44 +0800

    chore: Add constructor and builder to QueryOptions

--- src/main/java/com/happy3w/persistence/core/assistant/QueryOptions.java
+++ src/main/java/com/happy3w/persistence/core/assistant/QueryOptions.java
@@ -1,10 +1,16 @@
 package com.happy3w.persistence.core.assistant;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class QueryOptions {
     private long maxSize = -1;
 }
