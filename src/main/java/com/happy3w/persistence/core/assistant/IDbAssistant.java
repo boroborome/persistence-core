0d3af743ff4078505edb3ff8dcb706ffebfb09d1
Yushan Gao <ysgao@thoughtworks.com>
2021-01-02 11:07:34 +0800

    chore: add findById to IDbAssistant

--- src/main/java/com/happy3w/persistence/core/assistant/IDbAssistant.java
+++ src/main/java/com/happy3w/persistence/core/assistant/IDbAssistant.java
@@ -5,8 +5,13 @@ import com.happy3w.persistence.core.filter.IFilter;
 import java.util.List;
 import java.util.stream.Stream;
 
+/**
+ * 访问数据库的助手。
+ * 目标是完成任意类型数据的读写查。主要特点：单个数据类型的批量操作
+ */
 public interface IDbAssistant {
     <T> T saveData(T data);
     <T> void saveStream(Stream<T> dataStream);
+    <T> T findById(Class<T> dataType, Object id);
     <T> Stream<T> queryStream(Class<T> dataType, List<IFilter> filters, QueryOptions options);
 }
