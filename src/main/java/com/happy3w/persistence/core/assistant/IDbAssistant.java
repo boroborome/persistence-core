678948f6a2f586ed2fc9e6c8cf53d901854e2289
Yushan Gao <ysgao@thoughtworks.com>
2021-01-02 12:38:35 +0800

    chore: add more interface to IDbAssistant

--- src/main/java/com/happy3w/persistence/core/assistant/IDbAssistant.java
+++ src/main/java/com/happy3w/persistence/core/assistant/IDbAssistant.java
@@ -9,9 +9,11 @@ import java.util.stream.Stream;
  * 访问数据库的助手。
  * 目标是完成任意类型数据的读写查。主要特点：单个数据类型的批量操作
  */
-public interface IDbAssistant {
+public interface IDbAssistant<KT> {
     <T> T saveData(T data);
     <T> void saveStream(Stream<T> dataStream);
-    <T> T findById(Class<T> dataType, Object id);
-    <T> Stream<T> queryStream(Class<T> dataType, List<IFilter> filters, QueryOptions options);
+    <T> T findById(Class<T> dataType, KT id);
+    <T> Stream<T> findByFilter(Class<T> dataType, List<IFilter> filters, QueryOptions options);
+    <T> T deleteById(Class<T> dataType, KT id);
+    <T> long deleteByFilter(Class<T> dataType, List<IFilter> filters, QueryOptions options);
 }
